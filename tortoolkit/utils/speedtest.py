97fe6022c664bc49338c5ad1732b0ffa3a98078f
EDM 1.15 <82015596+EDM115@users.noreply.github.com>
2022-01-26 13:16:33 +0100

    Add files via upload

---
+++ tortoolkit/utils/speedtest.py
@@ -0,0 +1,28 @@
+from speedtest import Speedtest
+import logging
+from .human_format import human_readable_bytes
+
+torlog = logging.getLogger(__name__)
+
+async def get_speed(message):
+    imspd = await message.reply("`Running speedtest…`")
+    test = Speedtest()
+    test.get_best_server()
+    test.download()
+    test.upload()
+    test.results.share()
+    result = test.results.dict()
+    path = (result['share'])
+    string_speed = f'''
+**🏃Speedtest Result : **
+🏷️ Server Name : `{result["server"]["name"]}`
+📍 Country : `{result["server"]["country"]}, {result["server"]["cc"]}`
+💸 Sponsor : `{result["server"]["sponsor"]}`
+📤 Upload : `{human_readable_bytes(result["upload"] / 8)}/s`
+📥 Download : `{human_readable_bytes(result["download"] / 8)}/s`
+🏓 Ping : `{result["ping"]} ms`
+🌐 ISP : `{result["client"]["isp"]}`
+'''
+    await imspd.delete()
+    await message.reply(string_speed, parse_mode="markdown")
+    torlog.info(f'Server Speed result : \n📥 DL : {human_readable_bytes(result["download"] / 8)}/s 📤 UL : {human_readable_bytes(result["upload"] / 8)}/s')
